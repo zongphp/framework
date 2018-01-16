8c9e2c45780e2b81400771a4074642874bc6b2c5
zmingzhi <zmingzhi@yeah.net>
2018-01-16 09:48:49 +0800

    restore 1.x

--- src/Version.php
+++
@@ -1,9 +0,0 @@
-<?php namespace zongphp\framework;
-
-trait Version
-{
-    static public function version()
-    {
-        defined('ZONGPHP_VERSION') or define('ZONGPHP_VERSION', '2.0.138');
-    }
-}
